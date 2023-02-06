2c570911498e2da8f12bcd3b0e2cc0d6829dad8d
DanielYuanxk <dyxk1234@hotmail.com>
2023-02-06 09:38:29 -0500

    add comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,7 +3,7 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
+// rendering new tweet object for data at /tweeter
 const createTweetElement = function (tweetObj) {
   const $tweet = $(`<article>
             <header>
@@ -29,7 +29,7 @@ const createTweetElement = function (tweetObj) {
           </article>`);
   return $tweet;
 };
-
+//generating new tweets after they are successfully posted to the /tweeter page
 const renderTweets = function (tweetArray) {
   for (let tweet of tweetArray) {
     const newTweet = createTweetElement(tweet);
@@ -39,19 +39,24 @@ const renderTweets = function (tweetArray) {
 };
 
 $(() => {
+  //load the rendered tweets
   const loadTweets = function () {
     $.get("/tweets", function (data) {
       renderTweets(data);
     });
   };
   loadTweets();
+  // fetching tweets
   const refatchTweets = function () {
     $.get("/tweets", function (data) {
       const newTweet = createTweetElement(data[data.length - 1]);
       $("#tweetPost").prepend(newTweet);
     });
   };
+  //events to occur on submit
   $("#tweet-box").submit(function (event) {
+    //check for errors and give warnig when tweet characters exceed 140
+    //and when no characters at all.
     if ($("#tweet-text").val().length > 140) {
       $("#warning").slideDown();
       event.preventDefault();
@@ -66,10 +71,13 @@ $(() => {
       event.preventDefault();
       $("#warning").slideUp();
       $("#warning2").slideUp();
-
+      //converts text into javascript string
       const serialData = $("#tweet-text").serialize();
+      // posting user input to /tweets
       $.post("/tweets", serialData, function (data) {
+        //reseting text-box to new
         $("#tweet-text").val("");
+        //reseting character counter to 140
         $("#counter").text("140");
         refatchTweets();
       });
